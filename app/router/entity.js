8ff5786a6d4cb8fe8cdd91ff0918ed85837f4688
Тимофей <timofey.devkeeper@gmail.com>
2019-12-20 15:59:02 +0300

    create basic Entity CRUD

--- app/router/entity.js
+++ app/router/entity.js
@@ -4,48 +4,64 @@ const router = new Router();
 
 const Entity = require('../models/entity');
 
-/**
- * Get list
- */
+// get all records
 router.get('/entity', async (ctx) => {
-    ctx.set('Access-Control-Expose-Headers', 'X-Total-Count');
-    ctx.set('X-Total-Count', 2);
-    ctx.body = [
-        {
-            id: 1,
-            name: 'sdgs',
-            address: 'fsdfsd',
-            phone: '4235235',
-        },
-        {
-            id: 2,
-            name: 'sdgs',
-            address: 'fsdfsd',
-            phone: '4235235',
-        },
-    ]
+
+	console.log(ctx.request.query);
+
+	const q = ctx.request.query,
+		options = {};
+	if (q._sort) {
+		const sort = q._sort === 'id' ? '_id' : q._sort,
+			order = q._order === 'DESC' ? -1 : 1;
+		options.sort = {[sort]: order}    ;
+	}
+
+	options.skip = Number(q._start);
+
+	if (q._end) {
+		options.limit = q._end - options.skip;
+	}
+
+	const list = await Entity.find(null, null, options).exec();
+
+	const count = await Entity.count(null, null, options).exec();
+
+	ctx.set('Access-Control-Expose-Headers', 'X-Total-Count');
+	ctx.set('X-Total-Count', count);
+	ctx.body = list
 });
 
-/**
- * Get one record
- */
+// get one record by id
 router.get('/entity/:id', async (ctx) => {
-    /**
-     * @todo 
-     */
-    ctx.body = []
+	/**
+	 * @todo 
+	 */
+	//console.log(ctx.params.id);
+	const query = Entity.findById(ctx.params.id);
+	const record = await query.exec();
+	ctx.body = record;
 });
 
-/**
- * Create record
- */
+// create new record
 router.post('/entity', async (ctx) => {
 
-    let entity = new Entity({...ctx.request.body});
-    await entity.save();
+	let entity = new Entity({...ctx.request.body});
+	await entity.save();
 
-    ctx.body = entity;
+	ctx.body = entity;
 });
 
+// update record
+router.put('/entity/:id', async (ctx) => {
+	const entity = await Entity.findByIdAndUpdate(ctx.params.id, {...ctx.request.body});
+	ctx.body = entity;
+});
+
+// delete record
+router.delete('/entity/:id', async (ctx) => {
+	const entity = await Entity.findByIdAndDelete(ctx.params.id);
+	ctx.body = entity
+});
 
 module.exports = router;
