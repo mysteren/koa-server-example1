eacedd2b70f14d0e1e3e00c8fe7b072a4da668f0
Тимофей <timofey.devkeeper@gmail.com>
2019-12-25 14:06:29 +0300

    add project's CRUD and model

---
+++ app/models/project.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
+
+autoIncrement.initialize(mongoose.connection);
+
+const MeasureSchema = new mongoose.Schema({
+	_id: Number,
+  name: String,
+  object_code: Number,
+  contract_number: Number,
+  contract_date: Date,
+  ivestor_id: {
+    type: Number,
+    required: false, 
+    ref: 'Entity'
+  },
+  customer_id: {
+    type: Number,
+    required: true, 
+    ref: 'Entity'
+  },
+  contractor_id: {
+    type: Number,
+    required: true, 
+    ref: 'Entity'
+  },
+  subcontractors: [
+    {
+      type: Number,
+      ref: 'Entity'
+    }
+  ]
+
+}, {
+	versionKey: false,
+	toJSON: {
+		transform: function(doc, ret) {
+			ret.id = ret._id;
+			delete ret._id;
+		}
+	}
+});
+
+MeasureSchema.plugin(autoIncrement.plugin, {model: 'Measure', startAt: 1});
+
+module.exports = mongoose.model('Project', MeasureSchema); 
--- app/router/project.js
+++ app/router/project.js
@@ -1,13 +1,69 @@
 const Router = require('koa-router');
 const router = new Router();
 
+const Project = require('../models/project');
+
 // get all records
 router.get('/project', async (ctx) => {
 
+	const q = ctx.request.query,
+		filter = {}
+		options = {};
+
+	if (q._sort) {
+		const sort = q._sort === 'id' ? '_id' : q._sort,
+			order = q._order === 'DESC' ? -1 : 1;
+		options.sort = {[sort]: order};
+	}
+
+	options.skip = Number(q._start);
+
+	if (q._end) {
+		options.limit = q._end - options.skip;
+	}
+
+	if (q.id) {
+		filter._id = q.id;
+	}
+
+	const list = await Project.find(filter, null, options).exec();
+	const count = await Project.count(filter, null, options).exec();
+
 	ctx.set('Access-Control-Expose-Headers', 'X-Total-Count');
-	ctx.set('X-Total-Count', 0);
-	ctx.body = []
+	ctx.set('X-Total-Count', count);
+	ctx.body = list;
 });
 
+// get one record by id
+router.get('/project/:id', async (ctx) => {
+	/**
+	 * @todo 
+	 */
+	//console.log(ctx.params.id);
+	const query = Project.findById(ctx.params.id);
+	const record = await query.exec();
+	ctx.body = record;
+});
+
+// create new record
+router.post('/project', async (ctx) => {
+
+	let record = new Project({...ctx.request.body});
+	await record.save();
+
+	ctx.body = record;
+});
+
+// update record
+router.put('/project/:id', async (ctx) => {
+	const record = await Project.findByIdAndUpdate(ctx.params.id, {...ctx.request.body});
+	ctx.body = record;
+});
+
+// delete record
+router.delete('/project/:id', async (ctx) => {
+	const record = await Project.findByIdAndDelete(ctx.params.id);
+	ctx.body = record
+});
 
 module.exports = router;
