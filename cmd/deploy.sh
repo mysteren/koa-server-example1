d505aae4223955a60c21a2bc16415bb3b8554240
Тимофей <timofey.devkeeper@gmail.com>
2020-03-25 16:38:23 +0300

    fix deploy script

--- cmd/deploy.sh
+++ cmd/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -avz --delete --exclude='*node_modules/*' ./* timofey@91.109.206.101:/var/www/dordoc/api
-ssh timofey@91.109.206.101 'cd /var/www/dordoc/api && npm ic --production && sudo systemctl restart dordoc-api.service'
+rsync -avz --delete --exclude='node_modules/*' --exclude='config/local.js' ./* timofey@91.109.206.101:/var/www/dordoc/api
+ssh timofey@91.109.206.101 'cd /var/www/dordoc/api && npm ic --production && sudo /bin/systemctl restart dordoc-api.service'
